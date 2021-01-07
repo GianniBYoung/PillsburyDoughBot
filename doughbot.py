8a454ebcf4ef21d796ad79cfe25020bd576966eb
gianni <younggianniguy@gmail.com>
2021-01-06 21:15:23 -0600

    added watchpuppy to observe a directory for new files

--- doughbot.py
+++ doughbot.py
@@ -1,8 +1,15 @@
-import praw
+#using watchdog i can now store posts in the db if i would like to instead of a text file.
 import sqlite3
+import subprocess
+import os
+import praw
+import requests
 from config import *
 
-# to obtain correct fields flip the string first duhhh
+# /media/unit/ripme/AbsoluteUnits
+# /media/unit/ripme/AbsoluteUnits/FimonFogus_Hope_this_hasn_t_been_posted_before__cigdl2.jpg
+
+
 def reddit_authentication():
     return praw.Reddit(client_id=redditClientid,
                        client_secret=redditClientsecret,
@@ -16,14 +23,16 @@ def create_database():
     con.execute("PRAGMA foreign_keys = on")
     cursor = con.cursor()
 
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS Users(userId INTEGER PRIMARY KEY, name TEXT, allowedToPost INTEGER)"
-    )
+    cursor.execute("CREATE TABLE IF NOT EXISTS Users \
+         (userId INTEGER PRIMARY KEY, name TEXT, allowedToPost INTEGER)")
 
-    cursor.execute(
-        "CREATE TABLE IF NOT EXISTS Posts(id integer NOT NULL PRIMARY KEY, title text, author text, mediaPath text, FOREIGN KEY (author) REFERENCES Users(userId));"
-    )
+    cursor.execute("CREATE TABLE IF NOT EXISTS Posts \
+         (id INTEGER NOT NULL PRIMARY KEY, title text, author TEXT, mediaPath TEXT, \
+         FOREIGN KEY (author) REFERENCES Users(userId));")
 
+    cursor.execute("CREATE TABLE IF NOT EXISTS Subreddits \
+         (id INTEGER NOT NULL PRIMARY KEY, name TEXT, allowsCrossPosts INTEGER);"
+                   )
     con.commit()
 
 
@@ -31,8 +40,9 @@ def insert_user(name="Unknown"):
     con = sqlite3.connect('main.db')
     cursor = con.cursor()
 
-    cursor.execute('''INSERT INTO Users(name, allowedToPost) VALUES (?,?)''',
-                   (name, -1))
+    cursor.execute(
+        '''INSERT INTO Users(name, allowedToPost) \
+                      VALUES (?,?)''', (name, -1))
     con.commit()
 
 
@@ -41,11 +51,68 @@ def insert_post(authorKey, mediaPath, title="Unknown"):
     cursor = con.cursor()
 
     cursor.execute(
-        '''INSERT INTO Posts(title, author, mediaPath) VALUES (?,?,?)''',
-        (title, authorKey, mediaPath))
+        '''INSERT INTO Posts(title, author, mediaPath) \
+           VALUES (?,?,?)''', (title, authorKey, mediaPath))
     con.commit()
 
 
-create_database()
-insert_user("Gianni")
-insert_post(1, "ths/is/a/path")
+def insert_subreddit(name):
+    con = sqlite3.connect('main.db')
+    cursor = con.cursor()
+
+    cursor.execute(
+        '''INSERT INTO Subreddits(name, allowsCrossPosts) \
+           VALUES (?,?)''', (name, 1))
+    con.commit()
+
+
+def deconstruct_path(mediaPath):
+    combinedPath = basePath + mediaPath
+
+
+#TODO grab fileExtension
+#can use ends with
+def upload_to_imgur(title, imagePath):
+    fileExtension = "the file ext"
+    if fileExtension == 'mp4':
+        fileType = 'video'
+    else:
+        fileType = 'image'
+
+    try:
+        url = "https://api.imgur.com/3/upload"
+        payload = {'title': title}
+        files = [(fileType, open(imagePath, 'rb')),
+                 ('type', open(imagePath, 'rb'))]
+        headers = {'Cookie': imgurCookie}
+
+        response = requests.request("POST",
+                                    url,
+                                    headers=headers,
+                                    data=payload,
+                                    files=files)
+        return response
+    except:
+        print("Unable to upload to imgur")
+
+
+def get_media_paths():
+    postsTxt = open(pathToPosts, 'w')
+    for subdir, dirs, files in os.walk(basePath):
+        for filename in files:
+            filepath = subdir + os.sep + filename
+
+            if filepath.endswith(".jpg") or filepath.endswith(
+                    ".png") or filepath.endswith(".gif") or filepath.endswith(
+                        ".mp4"):
+                postsTxt.write(filepath + '\n')
+
+    postsTxt.close()
+
+
+
+#create_database()
+#insert_user("Gianni")
+#insert_post(1, "ths/is/a/path")
+#insert_subredditt("AbsoluteUnits")
+get_media_paths()
---
+++ watchpuppy.py
@@ -0,0 +1,44 @@
+#derived from watchpuppy by Davide Mastromatteo
+#http://thepythoncorner.com/dev/how-to-create-a-watchdog-in-python-to-look-for-filesystem-changes/
+import time
+from config import  pathToPosts
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+#PATHTOBEOBSERVED = basePath
+
+def on_created(event):
+    postsTxt = open(pathToPosts, 'a')
+    filePath = event.src_path
+    if str(filePath).endswith(('.png','jpg','pdf','gif','wav','mkv','mp4')):
+        postsTxt.write(str(filePath) + '\n')
+        print(f"{event.src_path} has been added!")
+
+    postsTxt.close()
+
+
+if __name__ == "__main__":
+
+    patterns = "*" 
+    ignore_patterns = ""
+    ignore_directories = False
+    case_sensitive = True
+    my_event_handler = PatternMatchingEventHandler(patterns, ignore_patterns,
+                                                   ignore_directories,
+                                                   case_sensitive)
+
+    my_event_handler.on_created = on_created
+
+    path = PATHTOBEOBSERVED
+    go_recursively = True
+
+    my_observer = Observer()
+    my_observer.schedule(my_event_handler, path, recursive=go_recursively)
+
+    my_observer.start()
+    try:
+        while True:
+            time.sleep(0.3)
+    except KeyboardInterrupt:
+        my_observer.stop()
+    my_observer.join()
